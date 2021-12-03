7c4c1c5036d317842ba27022a0e05d4a179f9886
tupa1 <tupa1@runsystem.net>
2021-12-03 13:55:25 +0700

    Initial Commit

---
+++ login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by GMO on 12/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by GMO on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
